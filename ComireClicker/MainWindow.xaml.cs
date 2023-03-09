f374d3a8c9e669e650a706d9937bd3d3f8bbd79d
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2023-03-09 13:33:09 -0700

    aaaaaaaaaaaaaaaaaaaa

--- ComireClicker/MainWindow.xaml.cs
+++ ComireClicker/MainWindow.xaml.cs
@@ -21,6 +21,9 @@ namespace ComireClicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        public double cookieCount = 0;
+        public double gameMultiplier = 1;
+
         // A list of multipliers
         List<Multiplier> multipliers = new List<Multiplier>();
         
@@ -28,7 +31,6 @@ namespace ComireClicker
         Multiplier multiplyMeBby = new Multiplier("btnClick", "click", "", 1, 0, 1, 1, true);
 
         // Set the default game multiplier to 1
-        double gameMultiplier = 1;
 
 
         public MainWindow()
@@ -38,6 +40,7 @@ namespace ComireClicker
             CreateMultipliersInstances();
 
             lblGameMultiplier.Content = gameMultiplier;
+
 
             DispatcherTimer dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
@@ -51,15 +54,78 @@ namespace ComireClicker
 
     private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            // Parse the current amount of cookies
-            int.TryParse(lblCurrentCookies.Content.ToString(), out int currentAmount);
+
+            // Reset the game multiplier back to zero
+            gameMultiplier = 0;
+
+            // Iterate over all the multipliers
+            foreach (var multiplier in multipliers)
+            {
+                // Store a local multiplier
+                double currentMulti = 0;
+                // Check if the multiplier is unlocked
+                if (multiplier.IsUnlocked)
+                {
+                    // Increase the multiplier by multiplying the number in game by the game multiplier.
+                    currentMulti += (multiplier.NumInGame * multiplier.GameMultiplier);
+                }
+                // Increase the game multiplier
+                gameMultiplier += currentMulti;
+            }
+
+            // Update the multiplyer amount
+
+            cookieCount += gameMultiplier;
+
+            // Set the current multiplier
+            lblGameMultiplier.Content = gameMultiplier;
+
 
             // Increase the cookies by the games current multiplier
-            lblCurrentCookies.Content = currentAmount + gameMultiplier;
+            lblCurrentCookies.Content = cookieCount;
+
+            // Updates the button state
+            UpdateButtonState();
+
+            
 
             // Forcing the CommandManager to raise the RequerySuggested event
             CommandManager.InvalidateRequerySuggested();
+
         }
+
+        private void UpdateButtonState()
+        {
+            foreach (var multiplier in multipliers)
+            {
+                // Find the button by name
+                Button button = (Button) listOfMultipliers.FindName(multiplier.Id);
+
+
+                if (button != null)
+                {
+                     MessageBox.Show(button.Name);
+                    // Enable/disable the button based on whether the player has enough cookies
+                    SetUpgradeButtonState(button, multiplier.GameCurrentAmount);
+                }
+            }
+
+            GenerateMultiperButtons();
+        }
+
+        private void SetUpgradeButtonState(Button button, double upgradeCost)
+        {
+            if (cookieCount >= upgradeCost)
+            {
+                button.IsEnabled = true;
+            }
+            else
+            {
+                button.IsEnabled = false;
+            }
+        }
+
+
 
 
         private void CreateMultipliersInstances()
@@ -67,19 +133,19 @@ namespace ComireClicker
             // CLEAR THE ARRAY FOR UR MOMS PURPOSES
             multipliers.Clear();
                                          //  id,       name,        imageURI,     gameMultiplier, startCost, costIncrease, numInGame, isUnlocked
-            multipliers.Add(new Multiplier("btnMaple", "Maple-Leaf", "MapleLeaf", 0.1,             0,        10,             0, true));
-            multipliers.Add(new Multiplier("btnShull", "Shull-er", "", 1, 100, 15, 0, true));
-            multipliers.Add(new Multiplier("btnCox", "Cox-ifer", "", 5, 500, 50, 0, false));
-            multipliers.Add(new Multiplier("btnBear", "Beardall-er", "", 15, 1000, 100, 0, false));
-            multipliers.Add(new Multiplier("btnFox", "Fox-ifyer", "", 25, 1500, 300, 0, false));
-            multipliers.Add(new Multiplier("btnBurk", "Berk-inator", "", 50, 5000, 600, 0, false));
-            multipliers.Add(new Multiplier("btnCantera", "Cantera-ina", "", 100, 5000, 600, 0, false));
-            multipliers.Add(new Multiplier("btnKohler", "Kohler-inator-3000", "", 150, 5000, 600, 0, false));
-            multipliers.Add(new Multiplier("btnSarah", "Sarah-bella", "", 250, 5000, 600, 0, false));
-            multipliers.Add(new Multiplier("btnMarshall", "Marshall-ina", "", 500, 5000, 600, 0, false));
-            multipliers.Add(new Multiplier("btnPritchard", "Pritchard-nator", "", 1000, 5000, 600, 0, false));
-            multipliers.Add(new Multiplier("btnParker", "Parker-Dator", "", 12000, 5000, 600, 0, false));
-            multipliers.Add(new Multiplier("btnComireTwo", "Coal-Miner", "", 15000, 5000, 600, 0, false));
+            multipliers.Add(new Multiplier("btnMaple", "Maple-Leaf", "Maple", 0.1,             0,        10,             0, true));
+            multipliers.Add(new Multiplier("btnShull", "Shull-er", "Shull", 1, 100, 15, 0, false));
+            multipliers.Add(new Multiplier("btnCox", "Cox-ifer", "Cox", 5, 500, 50, 0, false));
+            multipliers.Add(new Multiplier("btnBear", "Beardall-er", "Beardall", 15, 1000, 100, 0, false));
+            multipliers.Add(new Multiplier("btnFox", "Fox-ifyer", "Fox", 25, 1500, 300, 0, false));
+            multipliers.Add(new Multiplier("btnBurk", "Burk-inator", "Burk", 50, 5000, 600, 0, false));
+            multipliers.Add(new Multiplier("btnCantera", "Cantera-ina", "Cantera", 100, 5000, 600, 0, false));
+            multipliers.Add(new Multiplier("btnKohler", "Kohler-inator-3000", "Kohler", 150, 5000, 600, 0, false));
+            multipliers.Add(new Multiplier("btnSarah", "Sarah-bella", "Sarah", 250, 5000, 600, 0, false));
+            multipliers.Add(new Multiplier("btnMarshall", "Marshall-ina", "Marshall", 500, 5000, 600, 0, false));
+            multipliers.Add(new Multiplier("btnPritchard", "Pritchard-nator", "Comire", 1000, 5000, 600, 0, false));
+            multipliers.Add(new Multiplier("btnParker", "Parker-Dator", "Comire", 12000, 5000, 600, 0, false));
+            multipliers.Add(new Multiplier("btnComireTwo", "Coal-Miner", "Comire", 15000, 5000, 600, 0, false));
 
             GenerateMultiperButtons();
 
@@ -106,7 +172,7 @@ namespace ComireClicker
                 grid.ColumnDefinitions.Add(colDilf3);
 
                 Image multiplierImg = new Image();
-                multiplierImg.Source = new BitmapImage(new Uri("Resources/Comire.png", System.UriKind.Relative));
+                multiplierImg.Source = new BitmapImage(new Uri("Resources/" + multiplier.ImageURI + ".png", System.UriKind.Relative));
                 multiplierImg.Height = 30;
                 //multiplierImg
                 Grid.SetColumn(multiplierImg, 0);
@@ -143,61 +209,32 @@ namespace ComireClicker
             // Gets the button clicked
             Button btnSwitch = (Button)sender;
 
-            // Checks the buttons name
-            switch (btnSwitch.Name)
+            // Iterates through the multipliers list
+            foreach (Multiplier multiplier in multipliers)
             {
-                case "btnMaple":
-                    // Gets the position, and increased the total in the game
-                    multipliers[0].NumInGame += 1;
-                    // 
-                    //gameMultiplier += multipliers[0].GameMultiplier;
-                    break;
-                default:
-                    break;
-            }
-
-            // Re-generate the game's buttons
-            GenerateMultiperButtons();
-
-            // Reset the game multiplier back to zero
-            gameMultiplier = 0;
-
-            // Iterate over all the multipliers
-            foreach (var multiplier in multipliers)
-            {
-                // Store a local multiplier
-                double currentMulti = 0;
-                // Check if the multiplier is unlocked
-                if (multiplier.IsUnlocked)
+                // Checks if the button name matches the multiplier ID
+                if (btnSwitch.Name == multiplier.Id)
                 {
-                    // Increase the multiplier by multiplying the number in game by the game multiplier.
-                    currentMulti += (multiplier.NumInGame * multiplier.GameMultiplier);
+                    // Increases the number of the multiplier in the game
+                    cookieCount -= multiplier.GameCurrentAmount;
+                    UpdateButtonState();
+                    multiplier.NumInGame += 1;
+                    break;
                 }
-                // Increase the game multiplier
-                gameMultiplier += currentMulti;
-            }
-            
-            // Update the multiplyer amount
-            lblGameMultiplier.Content = gameMultiplier;
-
+            }           
 
         }
+
 
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
             //multiplyMeBby.increaseCurrentAmount();
 
-            // Gets the current amount of cookies and assigns it to a numerical value
-            int.TryParse(lblCurrentCookies.Content.ToString(), out int currentAmount);
-            //lblTest.Content = currentAmount.ToString();
 
-            lblCurrentCookies.Content = (currentAmount + 1);
+            cookieCount += (int) multiplyMeBby.GameMultiplier;
+
+            lblCurrentCookies.Content = cookieCount;
         }
 
-        /*
-         * 
-         * ON CLICK
-         * multipler.increaseCurrentAmount();
-         */
     }
 }
